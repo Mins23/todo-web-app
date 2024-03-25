995eeb8720cdc7bd652894cc2a5de3568b8fe43c
nehmtech <nehmtech.ke@gmail.com>
2024-03-25 12:39:11 +0300

    First commit

---
+++ todo/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+from django.utils import timezone
+
+class Todo(models.Model):
+    task = models.CharField(max_length = 200)
+    completed = models.BooleanField(default = False)
+    created_at = models.DateTimeField(default = timezone.now)
+    due_at = models.DateTimeField()
+    
+    def __str__(self):
+        return self.task
